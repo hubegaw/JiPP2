79c82e0b19d25d688f1ee49e644b5bd52c45fb4b
Hubert Gawczyński <hubert.gawczynski@gmail.com>
2021-10-12 20:20:34 +0200

    cwiczenie 1

---
+++ cwiczenie1/allocation_basics/cmake-build-debug/CMakeFiles/cwiczenie1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cwiczenie1.dir/main.cpp.obj"
+  "cwiczenie1.exe"
+  "cwiczenie1.exe.manifest"
+  "cwiczenie1.lib"
+  "cwiczenie1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cwiczenie1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cwiczenie1/allocation_basics/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "cwiczenie 1" << endl;
+
+    return 0;
+}
